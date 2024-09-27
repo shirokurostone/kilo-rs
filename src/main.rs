d2cbefba1d1fcf2c282da64db44d2b481693406e
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2024-09-27 23:41:08 +0900

    add run()

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use std::io::{stdin, Error, Read};
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = run() {
+        eprintln!("{}", err);
+    }
+}
+
+fn run() -> Result<(), Error> {
+    let mut buf = [0u8; 1];
+
+    enable_raw_mode()?;
+
+    loop {
+        match stdin().read(&mut buf)? {
+            0 => break,
+            _ => match buf[0] as char {
+                '\x11' => break, // ctrl+q
+                c => {
+                    print!("{}\r\n", c);
+                }
+            },
+        }
+    }
+
+    disable_raw_mode()?;
+
+    Ok(())
 }
