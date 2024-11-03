e4213e5dccd95aef9e88c68e60cd58145a20da9b
shirokurostone <426017+shirokurostone@users.noreply.github.com>
2024-11-03 14:29:38 +0900

    remove EditorScreen reference from StatusBar

--- src/screen.rs
+++ src/screen.rs
@@ -341,20 +341,23 @@ impl Drawable for MessageBar {
 
 pub fn refresh_screen(screen: &EditorScreen, message_bar: &MessageBar) -> Result<(), Error> {
     let mut buf = String::new();
-    let status_bar = StatusBar {
+    let mut status_bar = StatusBar {
         component: Component {
             x: 0,
             y: screen.component.height,
             width: screen.component.width,
             height: 1,
         },
-        screen,
+        left_status: "".to_string(),
+        right_status: "".to_string(),
     };
 
     buf.push_str(ESCAPE_SEQUENCE_HIDE_CURSOR);
     buf.push_str(ESCAPE_SEQUENCE_MOVE_CURSOR_TO_FIRST_POSITION);
 
     screen.draw(&mut buf)?;
+    status_bar.set_left_status(screen);
+    status_bar.set_right_status(screen);
     status_bar.draw(&mut buf)?;
     message_bar.draw(&mut buf)?;
 
@@ -411,62 +414,76 @@ impl Drawable for EditorScreen {
     }
 }
 
-struct StatusBar<'a> {
+struct StatusBar {
     component: Component,
-    screen: &'a EditorScreen,
+    left_status: String,
+    right_status: String,
 }
 
-impl StatusBar<'_> {
+impl StatusBar {
     pub fn set_size(&mut self, x: usize, y: usize, width: usize, height: usize) {
         self.component.set_size(x, y, width, height);
     }
+
+    pub fn set_left_status(&mut self, screen: &EditorScreen) {
+        self.left_status = format!(
+            "{:<20} - {} lines {}",
+            screen
+                .buffer
+                .get_filepath()
+                .unwrap_or_else(|| "[No Name]".to_string()),
+            self.component.height,
+            if screen.buffer.is_dirty() {
+                "(modified)"
+            } else {
+                ""
+            }
+        );
+    }
+
+    pub fn set_right_status(&mut self, screen: &EditorScreen) {
+        self.right_status = format!(
+            "{} | {}/{}",
+            screen
+                .buffer
+                .get_file_type()
+                .map_or("no ft", |ft| ft.to_str()),
+            screen.cy + 1,
+            screen.buffer.len()
+        );
+    }
 }
 
-impl Drawable for StatusBar<'_> {
+impl Drawable for StatusBar {
     fn draw(&self, buf: &mut String) -> Result<(), Error> {
         let cursor = move_cursor(self.component.x, self.component.y);
         buf.push_str(&cursor);
 
         buf.push_str(ESCAPE_SEQUENCE_STYLE_REVERSE);
 
-        let status = format!(
-            "{:<20} - {} lines {}",
-            self.screen
-                .buffer
-                .get_filepath()
-                .unwrap_or_else(|| "[No Name]".to_string()),
-            self.component.height,
-            if self.screen.buffer.is_dirty() {
-                "(modified)"
-            } else {
-                ""
-            }
-        );
-
-        if self.component.width < status.len() {
-            let s: String = status.chars().take(self.component.width).collect();
+        if self.component.width < self.left_status.len() {
+            let s: String = self
+                .left_status
+                .chars()
+                .take(self.component.width)
+                .collect();
             buf.push_str(&s);
         } else {
-            buf.push_str(&status);
+            buf.push_str(&self.left_status);
 
-            let right_status = format!(
-                "{} | {}/{}",
-                self.screen
-                    .buffer
-                    .get_file_type()
-                    .map_or("no ft", |ft| ft.to_str()),
-                self.screen.cy + 1,
-                self.screen.buffer.len()
-            );
-            if self.component.width as isize - status.len() as isize - right_status.len() as isize
+            if self.component.width as isize
+                - self.left_status.len() as isize
+                - self.right_status.len() as isize
                 > 0
             {
-                for _ in 0..(self.component.width - status.len() - right_status.len()) {
+                for _ in
+                    0..(self.component.width - self.left_status.len() - self.right_status.len())
+                {
                     buf.push(' ');
                 }
-                buf.push_str(&right_status);
+                buf.push_str(&self.right_status);
             } else {
-                for _ in 0..(self.component.width - status.len()) {
+                for _ in 0..(self.component.width - self.left_status.len()) {
                     buf.push(' ');
                 }
             }
